8b1a0c7324300591a002c5722fd05772c756a450
XYC <ycx@YCdeMacBook-Pro.local>
2021-05-16 09:43:31 +0800

    增加通知扩展功能

---
+++ Pushpush/PushServiceExt/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  PushServiceExt
+//
+//  Created by YC X on 2021/5/16.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
